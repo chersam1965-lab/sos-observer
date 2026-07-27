01b6e96a30ecc8acce60e0d28a0b000cfb07e409
chersam1965-lab <Chersam1965@gmail.com>
2026-07-27 23:19:33 +0100

    Add mobile development environment

---
+++ scripts/check-env.sh
@@ -0,0 +1,31 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "========== GSOS =========="
+
+echo
+echo "Project:"
+pwd
+
+echo
+echo "Node:"
+node -v
+
+echo
+echo "NPM:"
+npm -v
+
+echo
+echo "Git:"
+git --version
+
+echo
+echo "Branch:"
+git branch
+
+echo
+echo "Status:"
+git status
+
+echo
+echo "Date:"
+date
