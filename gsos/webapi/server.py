40f59477b9fe5ce19ff75146053db2a3542766d3
chersam1965-lab <chersam1965@gmail.com>
2026-08-27 20:50:03 +0100

    GSOS: support HEAD health checks

--- gsos/webapi/server.py
+++ gsos/webapi/server.py
@@ -109,6 +109,17 @@ class Handler(BaseHTTPRequestHandler):
             "Content-Type"
         )
         self.end_headers()
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header(
+            "Content-Type",
+            "application/json; charset=utf-8"
+        )
+        self.send_header(
+            "Cache-Control",
+            "no-store"
+        )
+        self.end_headers()
 
     def do_GET(self):
 
